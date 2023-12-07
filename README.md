c31febf023a732cdf0156e836d0be03cfaa7add5
ajiteshshree <93392474+ajiteshshree@users.noreply.github.com>
2023-12-07 18:56:43 +0530

    Updated Project
    Edited README, added final report pdf and moved everything out of Simulation folder.

---
+++ README.md
@@ -0,0 +1,70 @@
+# Streaming Data Visualization 
+
+Scientists and researchers across diverse fields rely on visualization techniques to analyze simulation data. However, this endeavor often proves cumbersome due to the resource-intensive nature of simulations, demanding high computational power typically found in supercomputer. Unfortunately, access to supercomputers and other resources is not consistently available. To address this challenge, there emerged a pressing need for a solution that
+allows simulations to run on computers with high computational power while enabling scientists and researchers to visualize the results conveniently on any computer, irrespective of its computational capabilities. This project aims to bridge this gap by developing an efficient streaming data visualization system that accommodates large-scale simulations, providing a flexible and accessible platform for data analysis and interpretation
+
+## Our Approach
+We operate a weather simulation on a designated server, referred to as the Simulation server, which runs for multiple time steps. This simulation generates data that can be stored as a file for subsequent visualization and in-depth analysis.
+
+In lieu of saving the simulation data to a file, we have implemented a system to stream the data directly from the Simulation server to a separate server dedicated to visualization, known as the Visualization server. This data transfer is facilitated through the TCP/IP Socket Protocol. Subsequently, the Visualization server processes and presents the received data using the VTK library. This entire process is conducted for a user-specified number of time steps.
+
+## Code Explanation
+
+### Single Socket Approach:
+
+- In this approach, we generate the data for for the images for all timesteps and store it as a 3D array in the Simulation Server.
+
+- Then, we send the entire 3D array to the Visualization Server using only one TCP/IP Socket.
+
+- In the Visualization Server, the data for each time-step is visualized iteratively using a single generated VTK screen until the user-specified time-step limit is reached.
+
+### Multiple Socket Approach:
+
+- Data is generated for each time-step and stored as a 2D array in the Simulation Server and is transmitted to the Visualization Server via TCP/IP Socket. 
+
+- On the Visualization Server, the data is visualized through the creation of a VTK screen.
+
+- Subsequently, data for the next time step is generated on the Simulation Server, transmitted for visualization through another socket, and displayed on the Visualization Server using a new VTK screen each time.
+
+- This iterative process continues until the user-specified time-step limit is reached.
+
+## Running the Code
+
+### Installing Dependencies:
+
+- NumPy: `pip install numpy`
+- Socket: `pip install socket`
+- Time: `pip install time`
+
+- Simulation Server Specific:
+
+    - MPICH Installation: https://www.mpich.org/downloads/
+    - mpi4py Installation: `pip install mpi4py`
+
+- Visualization Server Specific:
+    - VTK Installation: `pip install vtk`
+
+### Running the Simulation:
+
+Explore the "Single Socket Approach" folder for utilizing a singular socket connection for streaming, or navigate to the "Multiple Socket Approach" folder to leverage multiple socket connections for streaming.
+
+1. First run the file `SimulationServer.py` using the following command: `python SimulationServer.py` on the server used for simulation.
+
+2. Specify the following on the Simulation Server: 
+    - Grid width and height.
+    - Simulation server's IP address. 
+        - run `hostname -I` in Linux Terminal.
+        - run `ipconfig` in Windows CMD, IP address specified under `IPv4 Address`.
+    - Port Number 
+        - used `5566` for successful run
+    - Number of time-steps.
+
+3. Now the simulation server will enter listen mode.
+
+4. Run file `VisualizationServer.py` using the following command: `python VisualizationServer.py` on the server used for visualization.
+
+5. Now, specify the simulation server's IP address and port number on this server.
+
+6. Window will open on the Visualization Server, where the visualization will take place for the specified time-steps.
+
+7. Upon completion of the visualization, the images corresponding to each time-step can be accessed within the subfolder named `visualization_pics`, located in the same directory as VisualizationServer.py.
---
+++ Single Socket Approach/SimulationServer.py
@@ -0,0 +1,177 @@
+import socket
+import pickle
+import struct
+import time
+import numpy as np
+from mpi4py import MPI
+import os
+import time
+
+tt_create_data = 0
+tt_sending_data = 0
+numSteps = 0
+NX = 0
+NY = 0
+
+server_ip = None
+server_port = None
+server_address = None
+
+DT = 60.0  # Time step in seconds
+DX = 1000.0  # Spatial step in meters
+U0 = 10.0  # Initial horizontal wind velocity (m/s)
+V0 = 5.0  # Initial vertical wind velocity (m/s)
+KX = 0.00001  # Diffusion coefficient for X-direction
+KY = 0.00001  # Diffusion coefficient for Y-direction
+KZ = 0.00001  # Diffusion coefficient for Z-direction
+
+def initializeField(field, numSteps, NX, NY):
+    for t in range(numSteps):
+        for i in range(NX):
+            for j in range(NY):
+                field[t][i][j] = int.from_bytes(os.urandom(8), byteorder='big') % 100
+
+
+def simulateWeather(field, rank, num_processes, numSteps, server_address, NX, NY):
+    
+    global tt_sending_data, tt_create_data
+
+    tempField = [[[0.0 for _ in range(NY)] for _ in range(NX)] for _ in range(numSteps)]
+
+    if rank == 0:
+
+        begin = time.time()
+        # tcp connection created and connected to client
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(server_address)
+        end = time.time()
+        tt_sending_data = tt_sending_data + end - begin
+
+        begin = time.time()
+        for t in range(numSteps):
+            # Advection
+            for i in range(NX):
+                for j in range(NY):
+                    i_prev = int((i - U0 * DT / DX + NX)) % NX
+                    j_prev = int((j - V0 * DT / DX + NY)) % NY
+                    tempField[t][i][j] = field[t][i_prev][j_prev]
+
+            # Diffusion
+            for i in range(NX):
+                for j in range(NY):
+                    laplacian = (field[t][(i + 1) % NX][j] + field[t][(i - 1 + NX) % NX][j]
+                                  + field[t][i][(j + 1) % NY] + field[t][i][(j - 1 + NY) % NY]
+                                  - 4 * field[t][i][j]) / (DX * DX)
+                    tempField[t][i][j] += (KX * laplacian + KY * laplacian) * DT
+
+            end = time.time()
+            tt_create_data = end - begin
+
+        # Listen for incoming connections
+        server_socket.listen(1)
+        print("Waiting for a connection...")
+
+        begin = time.time()
+        # Accept a connection
+        connection, client_address = server_socket.accept()
+        print("Connection from", client_address)
+
+        data_list=tempField[:]
+        data_array=np.array(data_list)
+        
+        # Serialize the NumPy array to bytes
+        data_bytes = data_array.tobytes()
+        connection.sendall(data_bytes)
+
+        # Close the connection
+        connection.close()  
+        server_socket.close()
+        end = time.time()
+        tt_sending_data = tt_sending_data + end - begin
+
+def main():
+
+    global tt_sending_data, tt_create_data, server_ip, server_port, server_address, numSteps, NX, NY
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    num_processes = comm.Get_size()
+
+    # Get user input for NX, NY, IP address, and port
+    NX = int(input("Enter the value for NX: "))
+    NY = int(input("Enter the value for NY: "))
+    server_ip = input("Enter the server IP address: ")
+    server_port = int(input("Enter the server port number: "))
+    server_address = (server_ip, server_port)
+    numSteps = int(input("Enter the number of time steps: "))
+
+
+    begin = time.time()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(server_address)
+    end = time.time()
+
+    tt_sending_data = tt_sending_data + end - begin
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print("Waiting for a connection to send number of time steps...")
+
+    begin = time.time()
+    # Accept a connection
+    connection, client_address = server_socket.accept()
+    print("Connection from", client_address)
+
+    numSteps_bytes = numSteps.to_bytes(4, byteorder= 'big')
+    NX_bytes = NX.to_bytes(4, byteorder= 'big')
+    NY_bytes = NY.to_bytes(4 , byteorder= 'big')
+    connection.sendall(numSteps_bytes)
+    connection.sendall(NX_bytes)
+    connection.sendall(NY_bytes)
+
+    connection.close()  
+    server_socket.close()
+    end = time.time()
+    tt_sending_data = tt_sending_data + end - begin
+
+
+    field = [[[0.0 for _ in range(NY)] for _ in range(NX)] for _ in range(numSteps)]
+
+    if rank == 0:
+        initializeField(field, numSteps,  NX, NY)
+
+    # Distribute the initial field to all processes
+    field = comm.bcast(field, root=0)
+
+    simulateWeather(field, rank, num_processes, numSteps, server_address, NX, NY)
+
+    if rank == 0:
+        print("Weather simulation completed.")
+
+
+if __name__ == "__main__":
+    main()
+
+    # tcp connection created and connected to client
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(server_address)
+
+    # Listen for incoming connections
+    server_socket.listen(1)
+    print("Waiting for a connection to send number of time steps...")
+
+    # Accept a connection
+    connection, client_address = server_socket.accept()
+    print("Connection from", client_address)
+
+    tt_getting_data_client_bytes = connection.recv(8)
+    tt_getting_data_client = struct.unpack('d', tt_getting_data_client_bytes)[0]
+
+    tt_visualize_data_client_bytes = connection.recv(8)
+    tt_visualize_data_client = struct.unpack('d', tt_visualize_data_client_bytes)[0]
+
+    connection.close()  
+    server_socket.close()
+    print("Total time to create, send-receive and visualize ({}*{}) 2D array for {} time steps is {}, {} and {} seconds.".format(NX, NY, numSteps, tt_create_data, tt_sending_data + tt_getting_data_client, tt_visualize_data_client))
